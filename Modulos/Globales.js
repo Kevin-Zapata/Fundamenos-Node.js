c7febb0dbe188580950ae40d87015c116f552189
Kevin-Zapata <kevin19981921@gmail.com>
2023-02-13 15:10:33 -0500

    Aprendiendo sobre modulos Globales

--- Modulos/Globales.js
+++ Modulos/Globales.js
@@ -1,1 +1,6 @@
-console.log(global)
+console.log(process)
+ setTimout//: Se encarga de llamar a otra funcion despues de un periodo de tiempo
+ SetInterval//: Llama a otra funcion cada intervalo de tiempo
+ setImmediate//: Equivalente a setTimeOut pero con tiempo 0
+ clearTimeOut//: Detiene un setTimeOut
+clearInterval//: detiene un setInterval
